0ba85e1bfecfa3831318a710dc3df41469396405
Sajjad Najafi <Sajjad@ethicadata.com>
2022-07-14 16:52:38 +0430

    Initial Commit

---
+++ Kiliaro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kiliaro
+//
+//  Created by Sajjad Najafi on 7/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
