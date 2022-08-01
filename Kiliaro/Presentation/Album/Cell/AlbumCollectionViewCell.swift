dbf5f24c280c3568d04b6eeaff48a5e36049209a
Sajjad Najafi <Sajjad@ethicadata.com>
2022-08-02 02:05:41 +0430

    Image size added

--- Kiliaro/Presentation/Album/Cell/AlbumCollectionViewCell.swift
+++ Kiliaro/Presentation/Album/Cell/AlbumCollectionViewCell.swift
@@ -12,17 +12,39 @@ class AlbumCollectionViewCell: UICollectionViewCell {
     
     var imageUrl: URL! {
         didSet {
-            imageView.kf.setImage(with: imageUrl)
+            imageView.kf.setImage(with: imageUrl) { [weak self] result in
+                guard let self = self else {
+                    return
+                }
+                
+                let path = ImageCache.default.cachePath(forKey: self.imageUrl.absoluteString)
+                guard let attribtues = try? FileManager.default.attributesOfItem(atPath: path),
+                      let fileSize = attribtues[FileAttributeKey.size] as? Int else {
+                    return
+                }
+                
+                self.sizeLabel.isHidden = false
+                self.sizeLabel.text = "\((Double(fileSize) / 1024 / 1024).rounded(toPlaces: 3)) MB"
+            }
+            
         }
     }
     
     lazy var imageView: UIImageView = {
         let iv = UIImageView()
         iv.translatesAutoresizingMaskIntoConstraints = false
-        iv.layer.cornerRadius = 5
-        iv.clipsToBounds = true
         iv.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0)
         return iv
+    }()
+    
+    lazy var sizeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = ThemeManager.currentTheme.buttonBgColor
+        label.textColor = ThemeManager.currentTheme.buttonTextColor
+        label.textAlignment = .center
+        label.isHidden = true
+        return label
     }()
     
     required init?(coder: NSCoder) {
@@ -36,13 +58,26 @@ class AlbumCollectionViewCell: UICollectionViewCell {
     }
     
     func commonInit() {
+        contentView.layer.cornerRadius = 5
+        contentView.clipsToBounds = true
+        
         contentView.addSubview(imageView)
+        contentView.addSubview(sizeLabel)
+        
         NSLayoutConstraint.activate([
             imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
             imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            
+            sizeLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            sizeLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            sizeLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            sizeLabel.heightAnchor.constraint(equalToConstant: 30)
         ])
     }
     
+    override func prepareForReuse() {
+        sizeLabel.isHidden = true
+    }
 }
--- Kiliaro/Utils/Extensions.swift
+++ Kiliaro/Utils/Extensions.swift
@@ -73,3 +73,10 @@ extension URL {
         return urlComponents.url!
     }
 }
+
+extension Double {
+    func rounded(toPlaces places: Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
