1c4cb0198ce0588d9e1629035dbdf99d754048e2
Sajjad Najafi <Sajjad@ethicadata.com>
2022-08-01 17:37:54 +0430

    Image size added for album

--- Kiliaro/Presentation/Album/AlbumView+Delegate.swift
+++ Kiliaro/Presentation/Album/AlbumView+Delegate.swift
@@ -7,6 +7,27 @@
 
 import UIKit
 
-extension AlbumView: UICollectionViewDelegate {
+extension AlbumView: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return viewModel.getImageSize(for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return viewModel.getMinimumLineSpacing()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return viewModel.getMinimuminterItemSpacing()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return viewModel.getInsetForSection()
+    }
 }
--- Kiliaro/ViewModel/AlbumViewModel.swift
+++ Kiliaro/ViewModel/AlbumViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by Sajjad Najafi on 8/1/22.
 //
 
-import Foundation
+import UIKit
 
 class AlbumViewModel: ViewModel {
     var useCase: AlbumUseCaseable
@@ -28,5 +28,25 @@ class AlbumViewModel: ViewModel {
     
     func numberOfItems() -> Int {
         return items.count
+    }
+    
+    func getImageSize(for indexPath: IndexPath) -> CGSize {
+        let width = (UIScreen.main.bounds.width - getMinimuminterItemSpacing() - getInsetForSection().left - getInsetForSection().right) / 2
+        let item = items[indexPath.row]
+        let targetHeight = item.resx * width / item.resy
+        
+        return CGSize(width: width, height: targetHeight)
+    }
+    
+    func getMinimumLineSpacing() -> CGFloat {
+        return 8
+    }
+    
+    func getMinimuminterItemSpacing() -> CGFloat {
+        return 8
+    }
+    
+    func getInsetForSection() -> UIEdgeInsets {
+        return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
     }
 }
